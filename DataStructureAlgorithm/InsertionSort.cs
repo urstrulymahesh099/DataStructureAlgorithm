3704828951e43715231ce6aa7e372ad65d347418
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-22 17:35:50 +0530

    [Mahesh]Add: InsertionSort

---
+++ DataStructureAlgorithm/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class InsertionSort
+    {
+        public void Sort(int[] binaryArr)
+        {
+            for(int i=0;i<binaryArr.Length;i++)
+            {
+                for(int j =0; j<i; j++)
+                {
+                    if (binaryArr[i].CompareTo(binaryArr[j]) > 0)
+                    {
+                        int temp = binaryArr[j];
+                        binaryArr[j] = binaryArr[i];
+                        binaryArr[i] = temp;
+                    }
+                }
+            }
+            foreach(var data in binaryArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DataStructureAlgorithm
 {
-    class program
+    class Program
     {
         public static void Main(string[] args)
         {
-            BinarySearch binary = new BinarySearch();
-            binary.ReadTextFile(@"E:\Bridgelabz\LinkedListProblems\DataStructureAlgorithm\NewFolder\BinarySearch.txt");
-            binary.BinarySearchOperation("Crazy");
+            Console.WriteLine("Welcome to Stack and Queue problem Statement");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1. Binary Search\n 2. Insertion Sorting  ");
+                Console.WriteLine("Enter above mention option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binary = new BinarySearch();
+                        binary.ReadTextFile(@"E:\Bridgelabz\LinkedListProblems\DataStructureAlgorithm\NewFolder\BinarySearch.txt");
+                        binary.BinarySearchOperation("Crazy");
+                        break;
+
+                    case 2:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] arr = { 6,2,7, 5, 4,8, 1, 3 };
+                        insertionSort.Sort(arr);
+                        break;
+
+                    default:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+
+            }
+
         }
     }
 }
