92dcda8d76582230cf8225bcf3488b63e4c451b3
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-22 17:11:58 +0530

    [Mahesh]Add: Binary Search

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BinarySearch
+    {
+        string[] binaryArr;
+        public void ReadTextFile(string filePath)
+        {
+            string words = File.ReadAllText(filePath);
+            binaryArr = words.Split(",");
+        }
+        public void BinarySearchOperation(string input)
+        {
+            foreach (var word in binaryArr)
+            {
+                if (word == input)
+                {
+
+                    Console.WriteLine("The word in the list using binary search");
+                    return;
+                }
+            }
+            Console.WriteLine("Element is not available in list of words");
+        }
+
+    }
+}
---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearch binary = new BinarySearch();
+            binary.ReadTextFile(@"E:\Bridgelabz\LinkedListProblems\DataStructureAlgorithm\NewFolder\BinarySearch.txt");
+            binary.BinarySearchOperation("Crazy");
+        }
+    }
+}
